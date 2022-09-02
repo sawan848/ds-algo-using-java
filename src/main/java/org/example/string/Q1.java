946cf0af2cea10bc98c1509d529d7ed71fbcca5d
sawan848 <ksawan802@gmail.com>
2022-09-02 21:12:24 +0530

    string document

--- src/main/java/org/example/string/Q1.java
+++ src/main/java/org/example/string/Q1.java
@@ -1,11 +1,42 @@
 package org.example.string;
-/*
+/**
  * 11:55 AM
  * 01-08-2022
  */
 
 public class Q1 {
+    static String []arr={"Now ","is","the","time","for","all","good","men",
+            "to","come","to","the","aid","of","their","country"};
     public static void main(String[] args) {
-        
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length ; j++) {
+                if (arr[j].compareTo(arr[i])<0){
+                    String t=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=t;
+                }
+            }
+
+            System.out.println(arr[i]);
+        }
     }
 }
+/** output
+ *
+ Now
+ aid
+ all
+ come
+ country
+ for
+ good
+ is
+ men
+ of
+ the
+ the
+ their
+ time
+ to
+ to
+ */

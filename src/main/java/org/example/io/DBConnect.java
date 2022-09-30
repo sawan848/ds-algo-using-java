b860c31b34295dc51461833b2f382ad84e3c722b
sawan848 <ksawan802@gmail.com>
2022-09-30 21:08:04 +0530

    mysql and oracle xe connection

--- src/main/java/org/example/io/DBConnect.java
+++ src/main/java/org/example/io/DBConnect.java
@@ -20,8 +20,41 @@ public class DBConnect {
      * @param username
      * @param password
      */
-    public void connectMySQL(String username,String password){
-//        Connection connection=DriverManager.getConnection();
+    public static void connectMySQL(String username,String password) throws SQLException {
+        String url="jdbc:mysql://localhost:3306/cognizant";
+
+        Connection connection=DriverManager.getConnection(url, username,password);
+        //method 1
+        String query="show tables";
+
+        Statement statement=connection.createStatement();      //creating statement
+        ResultSet resultSet=statement.executeQuery(query);   //executing statement
+
+        if (connection!=null){
+            System.out.println("connected");
+            while(resultSet.next()){
+                System.out.println(resultSet.getString(1));
+            }
+        }
+
+        //method 2
+        Properties properties=new Properties();
+        properties.put("user","root");
+        properties.put("password","hope");
+
+        Connection connection2=DriverManager.getConnection(url,properties);
+
+        System.out.println("*************************************");
+
+        System.out.println("connected by method 2");
+
+        Statement statement2=connection2.createStatement();      //creating statement
+        ResultSet resultSet2=statement2.executeQuery(query);
+
+        while(resultSet2.next()){
+            System.out.println(resultSet2.getString(1));
+        }
+
 
     }
 
@@ -30,11 +63,27 @@ public class DBConnect {
      * @param username
      * @param password
      */
-    public void connectOracleXE(String username,String password){
+    public static void connectOracleXE(String username,String password) throws ClassNotFoundException, SQLException {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        String url="jdbc:oracle:thin:@localhost:1521:XE";
+        Connection connection=DriverManager.getConnection(url,username,password);
+        String query="select * from tab";
+        Statement statement=connection.createStatement();
+        ResultSet resultSet=statement.executeQuery(query);
+        while (resultSet.next())
+            System.out.println(resultSet.getString(1));
+
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        String username="root";
+        String password="hope";
+        connectMySQL(username,password);
+
+        String user="c##rudra";
+        String passwd="rudra";
+        connectOracleXE(user,passwd);
 
     }
 }
